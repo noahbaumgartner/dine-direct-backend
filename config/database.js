9bba02ae9506c02d1a6d37b128488874dd38cbec
baumgnoa <baumgnoa@students.zhaw.ch>
2023-07-02 18:02:05 +0200

    changed name to dino

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
-  storage: "./dine-direct.db",
+  storage: "./dino.db",
 });
 
 module.exports = sequelize;
